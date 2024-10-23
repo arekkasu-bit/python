c56c3eb2341b7f30f2ce0713c33ae02dc7b3347b
arekkasu-bit <alexdrl575@gmail.com>
2024-10-23 17:46:45 -0500

    estruturas de datos

---
+++ SEMANA CUATRO/listas1.py
@@ -0,0 +1,9 @@
+# Que es
+# Para que sirven
+# Como funcionan
+# Diferencia entre listas diccionarios y tuplas
+
+lista = ["manzana", "banana", "naranja"]
+print(lista[0])  # Imprime "manzana"
+lista[1] = "uva"  # Cambia "banana" por "uva"
+print(lista)  # Imprime ['manzana', 'uva', 'naranja']
